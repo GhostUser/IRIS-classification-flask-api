ea1f1240affed0518bc21658764c4f3db8b70c50
Rahul Raj <byjupat@gmail.com>
2021-10-05 17:24:20 +0530

    corrected requests

--- request.py
+++ request.py
@@ -5,10 +5,14 @@ url = 'http://localhost:5000/api_predict'
 
 data = requests.post(url,json={'sepal length (cm)': 7, 'sepal width (cm)': 3, 'petal length (cm)': 4, 'petal width (cm)': 1.5})
 
+print(data.json())
+
+
+
+'''
+data=[[7,3,4,1.5]]
 j_data = json.dumps(data)
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=j_data, headers=headers)
+r = requests.post(url, data=j_data)
 print(r, r.text)
-
-
-
+'''
